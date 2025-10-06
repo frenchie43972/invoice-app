4a86d36eca0c10001bf379d317f5c1cf4574c3b3
Kris French <frenchie43972@gmail.com>
2025-10-06 16:15:58 +0900

    Initial scaffold and folder setup

--- invoice-app/src/main.js
+++ invoice-app/src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import App from './App.vue';
 
+const pinia = createPinia();
+
+pinia.use(piniaPluginPersistedstate);
+
+App.use(pinia);
 createApp(App).mount('#app');
